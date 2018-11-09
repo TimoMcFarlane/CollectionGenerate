284786ed411fdf1d12b160788e69dc107310c9ef
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2018-11-09 18:27:10 +0200

    Initial commit

---
+++ src/main/java/fi/vismaconsulting/collectiongenerator/CollectiongeneratorApplication.java
@@ -0,0 +1,12 @@
+package fi.vismaconsulting.collectiongenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectiongeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectiongeneratorApplication.class, args);
+	}
+}
