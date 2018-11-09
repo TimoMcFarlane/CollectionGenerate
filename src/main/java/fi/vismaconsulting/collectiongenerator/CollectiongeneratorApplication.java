46e45cec1c7903e59f2f27eb230636b232cabf74
Timo McFarlane <timo.mcfarlane@cs.tamk.fi>
2018-11-09 19:18:36 +0200

    Haloo

--- src/main/java/fi/vismaconsulting/collectiongenerator/CollectiongeneratorApplication.java
+++ src/main/java/fi/vismaconsulting/collectiongenerator/CollectiongeneratorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CollectiongeneratorApplication {
-
+	// Haloo
 	public static void main(String[] args) {
 		SpringApplication.run(CollectiongeneratorApplication.class, args);
 	}
